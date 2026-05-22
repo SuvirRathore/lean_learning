e08ee50d866d59ed5098ca06e5088d8ccde23fb8
Suvir <suvirrathore@gmail.com>
2026-05-22 18:10:58 +0100

    my solutions to 3.5

--- MIL_work/C03_Logic/S05_Disjunction.lean
+++ MIL_work/C03_Logic/S05_Disjunction.lean
@@ -33,6 +33,7 @@ example : x < |y| → x < y ∨ x < -y := by
   case inl h =>
     rw [abs_of_nonneg h]
     intro h; left; exact h
+
   case inr h =>
     rw [abs_of_neg h]
     intro h; right; exact h
@@ -58,19 +59,67 @@ example : x < |y| → x < y ∨ x < -y := by
 namespace MyAbs
 
 theorem le_abs_self (x : ℝ) : x ≤ |x| := by
-  sorry
+  cases le_or_gt 0 x
+  case inl h =>
+    rw [abs_of_nonneg h]
+
+  case inr h =>
+    rw [abs_of_neg h]
+    linarith
 
 theorem neg_le_abs_self (x : ℝ) : -x ≤ |x| := by
-  sorry
+  rw[← abs_neg x]
+  apply le_abs_self
 
 theorem abs_add (x y : ℝ) : |x + y| ≤ |x| + |y| := by
-  sorry
+  rcases le_or_gt 0 (x + y) with h | h
+  · rw [abs_of_nonneg h]
+    simp [le_abs_self x, le_abs_self y, add_le_add]
+  rw [abs_of_neg h]
+  rw [neg_add]
+  simp [neg_le_abs_self x, neg_le_abs_self y, add_le_add]
+
 
 theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
-  sorry
+  rcases le_or_gt 0 y with h | h
+  · rw [abs_of_nonneg h]
+    constructor
+    · intro h'
+      left
+      exact h'
+    · intro h''
+      rcases h'' with h''' | h'''
+      · exact h'''
+      · linarith
+  rw [abs_of_neg h]
+  constructor
+  · intro h1
+    right
+    exact h1
+  · intro h2
+    rcases h2 with h3 | h4
+    · linarith
+    · exact h4
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
-  sorry
+  rcases le_or_gt 0 x with h1 | h2
+  · rw [abs_of_nonneg h1]
+    constructor
+    · intro h3
+      constructor
+      · linarith
+      exact h3
+    · intro h4
+      rcases h4 with ⟨h5, h6⟩
+      exact h6
+  · rw [abs_of_neg h2]
+    constructor
+    · intro h7
+      constructor
+      · linarith
+      · linarith
+    · intro h8
+      linarith
 
 end MyAbs
 
@@ -91,23 +140,23 @@ example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
     apply dvd_mul_right
 
 example {z : ℝ} (h : ∃ x y, z = x ^ 2 + y ^ 2 ∨ z = x ^ 2 + y ^ 2 + 1) : z ≥ 0 := by
-  sorry
+  rcases h with ⟨ x,y, rfl | rfl ⟩ <;> linarith [sq_nonneg x, sq_nonneg y]
 
 example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  exact sq_eq_one_iff.mp h
 
 example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  apply sq_eq_sq_iff_eq_or_eq_neg.mp h
 
 section
 variable {R : Type*} [CommRing R] [IsDomain R]
 variable (x y : R)
 
 example (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  exact sq_eq_one_iff.mp h
 
 example (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  apply sq_eq_sq_iff_eq_or_eq_neg.mp h
 
 end
 
@@ -124,5 +173,18 @@ example (P : Prop) : ¬¬P → P := by
   contradiction
 
 example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by
-  sorry
 
+  constructor
+  · intro h
+    by_cases h' : P
+    · right
+      tauto
+    · left
+      tauto
+
+  · intro h
+    cases h
+    intro h'
+    contradiction
+    intro h'
+    assumption
